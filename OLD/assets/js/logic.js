72ffc95ac94de7be695ad601486198963f8b316a
rtm713 <rtm713@gmail.com>
2023-07-28 23:02:00 -0500

    changed files

---
+++ OLD/assets/js/logic.js
@@ -0,0 +1,15 @@
+
+var titleBox = document.querySelector('#headDiv');
+var titleLetters = ["W","e","l","c","o","m","e"];
+var stop = 0;
+
+var titleAnimation = setInterval(function() {
+    var letter = document.createElement('h1');
+    letter.textContent = titleLetters[stop];
+    titleBox.append(letter);
+    stop++;
+
+    if (stop === titleLetters.length) {
+        clearInterval(titleAnimation);
+    };
+},200);
